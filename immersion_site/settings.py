5bb2981368828468d9ba66520fb2d370eb630146
antunessjamba <158247982+antunessjamba@users.noreply.github.com>
2025-07-06 04:28:30 +0100

    actualizando o fundo

--- immersion_site/settings.py
+++ immersion_site/settings.py
@@ -109,7 +109,7 @@ USE_TZ = True
 # Static files
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Security settings
